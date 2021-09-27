202954276451f136b6c4131027f48e1e1eb9d565
Earniac <60632413+Earniac@users.noreply.github.com>
2021-09-27 16:04:00 -0500

    Update ArragingColors.java

--- ArragingColors.java
+++ ArragingColors.java
@@ -6,6 +6,12 @@ public static void main(String[] args){
   
     int k1 = scan.nextInt();
     int k2 = scan.nextInt();
+  
+    System.out.println(arrange(k1,k2));
+  
+}
+  
+    public static void arrange(int k1, k2){
     //All variations with 1 k2 and any number of k1 have 1 solution since dupes
     if (k2 == 1) {return 1;}
     //Variations with just 1 k1 always have k2 permutations
